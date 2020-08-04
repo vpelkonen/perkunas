42f0ebe37d380f9a41cbed5fdd7a998c425cf27e
vpelkonen <ville.pelkonen@qvik.fi>
2020-08-04 23:15:50 +0300

    Adjust colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,7 +1,5 @@
 export default {
-  blood: "#C94960",
-  raven: "#040309",
-  lightning: "#D5DAD9",
-  chainmail: "#343434",
-  dirt: "#61550E",
+  blood: "#E93940",
+  raven: "#030206",
+  lightning: "#EFEFEF",
 };
